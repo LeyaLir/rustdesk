e134aab031f991eb168a6b97b587570aa19c64b9
Баталин Иван Анатольевич <batalin@ao-nk.ru>
2024-11-14 11:39:05 +0500

    Replace servers

--- libs/hbb_common/src/config.rs
+++ libs/hbb_common/src/config.rs
@@ -100,8 +100,8 @@ const CHARS: &[char] = &[
     'm', 'n', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z',
 ];
 
-pub const RENDEZVOUS_SERVERS: &[&str] = &["rs-ny.rustdesk.com"];
-pub const PUBLIC_RS_PUB_KEY: &str = "OeVuKk5nlHiXp+APNn0Y3pC1Iwpwn44JGqrQCsWqmBw=";
+pub const RENDEZVOUS_SERVERS: &[&str] = &["pbx.ao-nk.ru"];
+pub const PUBLIC_RS_PUB_KEY: &str = "BNQoIY99+xmqsakgb0g4VryS2wRqYwi1RTmEYucWi4w=";
 
 pub const RS_PUB_KEY: &str = match option_env!("RS_PUB_KEY") {
     Some(key) if !key.is_empty() => key,
